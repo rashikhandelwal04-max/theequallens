a8d97f5a062e69ed7fde0214cec487e5066f1ae3
rashikhandelwal04-max <rashikhandelwal04@gmail.com>
2026-02-08 17:31:10 +0530

    Update Intro.js

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,3 +1,19 @@
 export default function Intro(){
-  return <h1>Intro works</h1>;
+  return (
+    <section style={{
+      minHeight:"100vh",
+      background:"#111",
+      color:"white",
+      display:"flex",
+      alignItems:"center",
+      justifyContent:"center",
+      textAlign:"center",
+      padding:"40px"
+    }}>
+      <div>
+        <h1>The Equal Lens</h1>
+        <p>We don’t lecture. We help them notice.</p>
+      </div>
+    </section>
+  );
 }

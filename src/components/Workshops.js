15f283c0a9c042fcccdb0a68dd40e4e33c460859
rashikhandelwal04-max <rashikhandelwal04@gmail.com>
2026-02-08 17:33:28 +0530

    Create Workshops.js

---
+++ src/components/Workshops.js
@@ -0,0 +1,15 @@
+export default function Workshops(){
+return (
+<section style={{background:"#ffe45c",padding:"120px 10vw"}}>
+<h2>Workshops</h2>
+
+<div style={{display:"grid",gridTemplateColumns:"1fr 1fr",gap:20}}>
+<img src="/images/workshop1.jpg" />
+<img src="/images/activity-ball.jpg" />
+<img src="/images/group-discussion.jpg" />
+<img src="/images/game-cards.jpg" />
+</div>
+
+</section>
+);
+}

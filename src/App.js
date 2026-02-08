4d925708ca8bb5b6980d28576e82d058e4b0e1dd
rashikhandelwal04-max <rashikhandelwal04@gmail.com>
2026-02-08 17:30:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import PhraseField from "./components/PhraseField";
 import Intro from "./components/Intro";
+import About from "./components/About";
+import Audience from "./components/Audience";
+import Workshops from "./components/Workshops";
+import MatchGame from "./components/MatchGame";
+import ScenarioGame from "./components/ScenarioGame";
+import Dashboard from "./components/Dashboard";
 
 export default function App(){
-  return <Intro />;
+  return (
+    <>
+      <PhraseField/>
+      <Intro/>
+      <About/>
+      <Audience/>
+      <Workshops/>
+      <MatchGame/>
+      <ScenarioGame/>
+      <Dashboard/>
+    </>
+  );
 }
